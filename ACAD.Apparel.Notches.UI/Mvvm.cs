896488fe716a9a775b2028fbf6937fa25943606d
Valery Vitko <valery.vitko@gmail.com>
2018-02-22 20:28:50 +0300

    Params UI draft

---
+++ ACAD.Apparel.Notches.UI/Mvvm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ACAD.Apparel.Notches.UI
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+}
---
+++ ACAD.Apparel.Notches.UI/ParamsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ACAD.Apparel.Notches.UI
+{
+    public class ParamsViewModel : ObservableObject
+    {
+        public double LengthSource { get; set; }
+        public double LengthDestination { get; set; }
+        public double Difference => LengthSource - LengthDestination;
+
+        public ObservableCollection<NotchFacetViewModel> Facets { get; set; } = new ObservableCollection<NotchFacetViewModel>();
+    }
+
+    public class NotchFacetViewModel : ObservableObject
+    {
+        public int FromNotch { get; set; }
+        public int ToNotch { get; set; }
+        public string Label => $"{FromNotch} - {ToNotch}";
+        public double Length { get; set; }
+        public double AdjustmentPercentage { get; set; } = 0;
+    }
+
+    internal class TestParamsViewModel : ParamsViewModel
+    {
+        public TestParamsViewModel()
+        {
+            LengthSource = 100.1;
+            LengthDestination = 110;
+
+            Facets.Add(new NotchFacetViewModel { FromNotch = 0, ToNotch = 1, Length = 5 });
+            Facets.Add(new NotchFacetViewModel { FromNotch = 1, ToNotch = 2, Length = 5.1 });
+        }
+    }
+}
